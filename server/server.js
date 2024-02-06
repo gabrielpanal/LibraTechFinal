52ee04d619a4b02fc1cbaa481d85a16acc3adb20
gabrielpanal <panalgab@gmail.com>
2024-02-06 19:56:41 +0800

    Update server.js

--- server/server.js
+++ server/server.js
@@ -340,6 +340,7 @@ app.put('/returnBook/:DeweyDec', (req, res) => {
     });
 });
 
-app.listen(5000, () => {
-    console.log("Server started on port 5000");
-})
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server started on port ${PORT}`);
+});
