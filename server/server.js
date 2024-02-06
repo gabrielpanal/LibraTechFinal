ead6a73a621682b7258dfc1073ceb11c58399f9b
gabrielpanal <panalgab@gmail.com>
2024-02-06 23:10:06 +0800

    Update server.js

--- server/server.js
+++ server/server.js
@@ -4,7 +4,11 @@ const cors = require('cors')
 
 const app = express()
 app.use(express.json())
-app.use(cors())
+app.use(cors({
+    origin: ["libra-tech-final-server.vercel.app"],
+    methods: ["POST", "GET"],
+    credentials: true
+}))
 
 // const db = mysql.createConnection({
 //     host: "localhost",
