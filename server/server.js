56e860e6ec07a25d51566d64cf57448b313cec89
gabrielpanal <panalgab@gmail.com>
2024-02-07 15:55:23 +0800

    server and vercel

--- server/server.js
+++ server/server.js
@@ -342,6 +342,8 @@ app.get("/", (req, res) => {
 //     });
 // });
 
-app.listen(5000, () => {
-    console.log("Server started on port 5000");
-})
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+    console.log(`Server started on port ${port}`);
+});
