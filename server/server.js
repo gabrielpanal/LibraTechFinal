4b66395b066a35b01322d8c5c6c8e8cdca9859e8
gabrielpanal <panalgab@gmail.com>
2024-02-07 15:59:11 +0800

    moved vercel

--- server/server.js
+++ server/server.js
@@ -342,8 +342,6 @@ app.get("/", (req, res) => {
 //     });
 // });
 
-const port = process.env.PORT || 5000;
-
-app.listen(port, () => {
-    console.log(`Server started on port ${port}`);
-});
+app.listen(5000, () => {
+    console.log("Server started on port 5000");
+})
